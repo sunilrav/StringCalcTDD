743a5c5caff76d27d1a34d4373b30007e129bf58
sunilrav <rsunilbabu@gmail.com>
2013-07-08 17:30:06 -0600

    Input of empty string returns zero

---
+++ StringCalcTDD.Tests/StringCalcTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalcTDD.Tests
+{
+    [TestClass]
+    public class StringCalcTests
+    {
+        [TestMethod]
+        public void InputOfEmptyStringWillReturnZero()
+        {
+            //Arrange
+            var stringCalculator = new StringCalculator();
+            //Act
+            var result = stringCalculator.Add("");
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+    }
+
+    public class StringCalculator
+    {
+        public int Add(string empty)
+        {
+            return 0;
+        }
+    }
+}
